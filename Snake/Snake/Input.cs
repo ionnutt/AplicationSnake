d3e9281d674e382b297ee028416d8c9c2aed5d75
ionut <ionut.b@codingheads.com>
2017-04-29 20:01:48 +0300

    create file settings circle and input

---
+++ Snake/Snake/Input.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Input
+    {
+        //Incarcam liste cu posibilele kayboard buttons
+        private static Hashtable keyTable = new Hashtable();
+
+        //Perform a check to see if   a particular  button  is pressed
+        public static bool KeyPressed(Keys key)
+        {
+            if(keyTable[key] == null)
+            {
+                return false;
+            }
+            return (bool)keyTable[key];
+        }
+
+        //Detect if a keyboard is pressed
+        public  static void ChangeState(Keys key, bool state)
+        {
+            keyTable[key] = state;
+        }
+    }
+}
---
+++ Snake/Snake/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Settings
+    {
+        public  int Width { get; set; }
+        public  int Height { get; set; }
+        public  int Speed { get; set;}
+        public  int Score { get; set; }
+        public  int Points { get; set; }
+        public bool GameOver { get; set; }
+    }
+}
